7ddbebb3316fbd8056946875a7283daafd262c47
stephen burns <steve@exp.org.au>
2017-01-31 13:38:07 +1100

    Added proto of ragdoll explosion.

---
+++ Assets/RagdollExplode.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollExplode : MonoBehaviour {
+
+    Rigidbody[] rigidBodies;
+
+	// Use this for initialization
+	void Start () {
+        rigidBodies = GetComponentsInChildren<Rigidbody>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.E))
+        {
+            Vector3 explosionPos = new Vector3(this.transform.position.x, this.transform.position.y -2, this.transform.position.z);
+            foreach (Rigidbody rb in rigidBodies)
+            {
+                rb.AddExplosionForce(10000, explosionPos, 100);
+            }
+        }
+	}
+}
